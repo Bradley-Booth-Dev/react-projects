5a016a9cf626527c137bd425da5b3c4ceb267183
Bradley Booth <bradley.booth@hotmail.co.uk>
2023-08-16 17:28:05 +0100

    started 11

---
+++ 11-navbar/setup/src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="Logo" />
+          <button className="nav-toggle"></button>
+        </div>
+        <div className="links-container show-container"></div>
+        <ul className="social-icons"></ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
